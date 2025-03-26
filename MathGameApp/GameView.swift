d05952885714376b9b5806e266887aa84636601f
alexandresamson <alexander_samson@icloud.com>
2025-03-26 13:54:33 +0100

    Ui geändert

---
+++ MathGameApp/GameView.swift
@@ -0,0 +1,8 @@
+//
+//  GameView.swift
+//  MathGameApp
+//
+//  Created by Alexandre Samson on 26.03.25.
+//
+
+import Foundation
---
+++ MathGameApp/HighscoreView.swift
@@ -0,0 +1,8 @@
+//
+//  HighscoreView.swift
+//  MathGameApp
+//
+//  Created by Alexandre Samson on 26.03.25.
+//
+
+import Foundation
